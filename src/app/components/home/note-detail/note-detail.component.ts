e024215f998d8ac6041c9253b7c9304325533155
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-05-29 03:57:16 -0400

    Add : [AN-10] setup template driven form

--- src/app/components/home/note-detail/note-detail.component.ts
+++ src/app/components/home/note-detail/note-detail.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Note } from 'src/app/models/note.model';
 
 @Component({
   selector: 'app-note-detail',
   templateUrl: './note-detail.component.html',
-  styleUrls: ['./note-detail.component.scss']
+  styleUrls: ['./note-detail.component.scss'],
 })
 export class NoteDetailComponent implements OnInit {
+  note = new Note('This is a test', 'This is a test note', 1);
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  logData(noteData: NgForm) {
+    console.log(noteData.form.value);
   }
-
 }
