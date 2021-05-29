6830bad13232a2e70938a96f51758e4e8fc72a11
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-05-29 03:57:29 -0400

    Update : [AN-8] note model to include id

--- src/app/models/note.model.ts
+++ src/app/models/note.model.ts
@@ -1,9 +1,11 @@
 export class Note {
+  id: number;
   title: string;
   content: string;
   dateAdded: Date;
   lastUpdated: Date;
-  constructor(title: string, content: string) {
+  constructor(title: string, content: string, id: number) {
+    this.id = id;
     this.title = title;
     this.content = content;
     this.dateAdded = new Date();
